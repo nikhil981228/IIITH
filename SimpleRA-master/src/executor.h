1307d82ec97bcba1d5a1afb424334cb055bdb7f7
Nikhil Chawla <81984809+nikhil981228@users.noreply.github.com>
2023-08-17 12:25:54 +0530

    Add files via upload

---
+++ SimpleRA-master/src/executor.h
@@ -0,0 +1,24 @@
+#include"semanticParser.h"
+
+void executeCommand();
+
+// 
+void doCommand();
+
+void executeCLEAR();
+void executeCROSS();
+void executeDISTINCT();
+void executeEXPORT();
+void executeINDEX();
+void executeJOIN();
+void executeLIST();
+void executeLOAD();
+void executePRINT();
+void executePROJECTION();
+void executeRENAME();
+void executeSELECTION();
+void executeSORT();
+void executeSOURCE();
+
+bool evaluateBinOp(int value1, int value2, BinaryOperator binaryOperator);
+void printRowCount(int rowCount);
